1342bbd59f8964fece4dcc2901f2760f9e1c815e
Adjel <adjel.touaoula@gmail.com>
2024-04-11 16:20:11 +0200

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,16 @@
 // src/app/page.js
-import React from 'react';
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
+import React from "react";
+import { readFile, writeFile } from "../helpers/file-helpers";
+export const dynamic = "force-dynamic";
 
-const DATABASE_PATH = '/src/database.json';
+const DATABASE_PATH = "/src/database.json";
 
 function Home() {
-  let { hits } = JSON.parse(
-    readFile(DATABASE_PATH)
-  );
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
 
   hits += 1;
 
-  writeFile(
-    DATABASE_PATH,
-    JSON.stringify({ hits })
-  );
+  writeFile(DATABASE_PATH, JSON.stringify({ hits }));
 
   return (
     <main>
